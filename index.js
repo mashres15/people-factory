64dca5fd4aeace23219895195eae20e67a0aed52
Maniz Shrestha <mashres15@gmail.com>
2017-06-05 15:07:21 -0400

    with picture

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ function handleSubmit(ev) {
   const name = f.personName.value
   const age = f.personAge.value
   const color = f.color.value
-  document.getElementById("personForm").style.borderColor = color
-  document.querySelector('h1').textContent = name + age 
+  document.getElementById('personForm').style.borderColor = color
+  document.querySelector('h1').textContent = name +" " + age 
+  document.getElementById('emptyPara').textContent = "Your Input - Name: " + name + ", Age: " + age + ", Color: " + color
 }
 
 personForm.addEventListener('submit', handleSubmit)
