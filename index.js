735aff605ad85a0c2dfb593b99f26c3a921e2857
Maniz Shrestha <mashres15@gmail.com>
2017-06-06 13:10:31 -0400

    Day 2 Lab

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 const personForm = document.querySelector('form#personForm')
 
 function handleSubmit(ev) {
-  ev.preventDefault()
-  const f = ev.target
-  const name = f.personName.value
-  const age = f.personAge.value
-  const color = f.color.value
-  document.getElementById('personForm').style.borderColor = color
-  document.querySelector('h1').textContent = name +" " + age 
-  document.getElementById('emptyPara').textContent = "Your Input - Name: " + name + ", Age: " + age + ", Color: " + color
+    ev.preventDefault()
+    const f = ev.target
+    const details = document.querySelector('#details')
+    const person ={
+        name: f.personName.value, 
+        age: f.personAge.value,
+        color: setColor(f.color.value).outerHTML,
+    }
+    document.getElementById('personForm').style.borderColor = f.color.value
+    document.querySelector('h1').textContent = f.personName.value + " " + f.personAge.value
+    document.getElementById('emptyPara').textContent = "Your Input - Name: " + f.personName.value + ", Age: " + f.personAge.value + ", Color: " + f.color.value
+
+    
+    details.appendChild(createList(person))
 }
 
 personForm.addEventListener('submit', handleSubmit)
 
+
+
+function createListElement(field, value) {
+    const li = document.createElement("li");
+    li.innerHTML = `${field}: ${value}`
+    return li
+}
+
+function createList(personData){
+    const ul = document.createElement('ul')
+    Object.keys(personData).map(function(field){
+        const li = createListElement(field, personData[field])
+        ul.appendChild(li)
+    })
+
+    return ul
+}
+
+function setColor(color){
+    const colorDiv = document.createElement('div')
+    colorDiv.style.backgroundColor = color
+    colorDiv.style.height = "50px"
+    colorDiv.style.width = "100px"
+
+    return colorDiv
+}
